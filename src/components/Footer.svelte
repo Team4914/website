8fe6993e60d4f7ff863ea5a470076b8087914b52
Sleepyleaf58 <frank.huang2@student.tdsb.on.ca>
2023-10-06 23:24:02 -0400

    Added aesthetic footer. Reponsiveness to be fixed

---
+++ src/components/Footer.svelte
@@ -0,0 +1,58 @@
+<!-- Footer Component-->
+
+<script lang="ts">
+    import '../app.css';
+
+    // Socials Links
+    const logoImage = "https://cdn.discordapp.com/attachments/1156357127011573840/1159979170386935808/Logo.png?ex=6532fdc9&is=652088c9&hm=099ded07c69c35a15e545fda0f451f2bda2cd4847fec044d1efbbefa17f021c6&"
+    const linkInstagram = "https://www.instagram.com/vpci.robotics/";
+    const linkEmail = "mailto:frcteam4914@gmail.com";
+</script>
+
+<!-- Footer wrapper-->
+<footer class="bg-base-100 text-slate-300">
+    <div class="p-5 bg-slate-800">
+        <!-- Resources-->
+        <div class="mb-10 flex sm:flex-column md:flex-row justify-evenly">
+            <!-- About -->
+            <div class="w-96">
+                <img class="w-24 bg-slate-700 float-left mr-4 rounded-xl" src="{logoImage}" alt="VP Robotics Logo" />
+                <h2 class="font-bold text-xl">Victoria Park Robotics</h2>
+                <p>Victoria Park Collegiate Institute</p>
+                <p>15 Wallingford Rd</p>
+                <p>Toronto, ON, CAN</p>
+            </div>
+            <!-- Phone -->
+            <div class="w-96">
+                <div class="w-24 h-24 flex justify-center align-center bg-slate-700 float-left mr-4 rounded-xl"><svg class="w-14 fill-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M164.9 24.6c-7.7-18.6-28-28.5-47.4-23.2l-88 24C12.1 30.2 0 46 0 64C0 311.4 200.6 512 448 512c18 0 33.8-12.1 38.6-29.5l24-88c5.3-19.4-4.6-39.7-23.2-47.4l-96-40c-16.3-6.8-35.2-2.1-46.3 11.6L304.7 368C234.3 334.7 177.3 277.7 144 207.3L193.3 167c13.7-11.2 18.4-30 11.6-46.3l-40-96z"/></svg></div>
+                <h2 class="font-bold text-xl">Phone</h2>
+                <p>(416) 395-3310</p>
+            </div>
+            <!-- Email -->
+            <div class="w-96">
+                <div class="w-24 h-24 flex justify-center align-center bg-slate-700 float-left mr-4 rounded-xl"><svg class="w-14 fill-white" xmlns="http://www.w3.org/2000/svg" height="6em" viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48H48zM0 176V384c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V176L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z"/></svg></div>
+                
+                <h2 class="font-bold text-xl">Email</h2>
+                <p><a href="{linkEmail}"></a>frcteam4914@gmail.com</p>
+            </div>
+        </div>
+        <!-- Divider-->
+        <hr class="bg-gray-200">
+        <!-- Icons - Expand and change color on hover-->
+        <div class="mt-5 flex flex-row flex-wrap justify-center gap-5 ">
+            <!-- Email -->
+            <a><svg class="fill-gray-400 hover:fill-white transition ease-in-out delay-150 hover:scale-110" xmlns="http://www.w3.org/2000/svg" height="1.5em" viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48H48zM0 176V384c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V176L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z"/></svg></a>
+            <!-- Instagram -->
+            <a><svg class="fill-gray-400 hover:fill-white transition ease-in-out delay-150 hover:scale-110" xmlns="http://www.w3.org/2000/svg" height="1.5em" viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"/></svg></a>
+            <!-- Youtube -->
+            <a><svg class="fill-gray-400 hover:fill-white transition ease-in-out delay-150 hover:scale-110" xmlns="http://www.w3.org/2000/svg" height="1.5em" viewBox="0 0 576 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M549.655 124.083c-6.281-23.65-24.787-42.276-48.284-48.597C458.781 64 288 64 288 64S117.22 64 74.629 75.486c-23.497 6.322-42.003 24.947-48.284 48.597-11.412 42.867-11.412 132.305-11.412 132.305s0 89.438 11.412 132.305c6.281 23.65 24.787 41.5 48.284 47.821C117.22 448 288 448 288 448s170.78 0 213.371-11.486c23.497-6.321 42.003-24.171 48.284-47.821 11.412-42.867 11.412-132.305 11.412-132.305s0-89.438-11.412-132.305zm-317.51 213.508V175.185l142.739 81.205-142.739 81.201z"/></svg></a>
+            <!-- TikTok -->
+            <a><svg class="fill-gray-400 hover:fill-white transition ease-in-out delay-150 hover:scale-110" xmlns="http://www.w3.org/2000/svg" height="1.5em" viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M448,209.91a210.06,210.06,0,0,1-122.77-39.25V349.38A162.55,162.55,0,1,1,185,188.31V278.2a74.62,74.62,0,1,0,52.23,71.18V0l88,0a121.18,121.18,0,0,0,1.86,22.17h0A122.18,122.18,0,0,0,381,102.39a121.43,121.43,0,0,0,67,20.14Z"/></svg></a>
+        </div>
+    </div>
+
+    <!-- Bottom section of footer-->
+    <div class="px-12 py-4 text-center bg-slate-950">
+        VPCI Robotics © 2023 All Rights Reserved.
+    </div>
+</footer>
