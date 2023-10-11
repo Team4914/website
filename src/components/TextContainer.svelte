e14b8205a31d472ce04bb48a346a3c3ad9644a1f
ShanmugarajahShajeenth <130683557+ShanmugarajahShajeenth@users.noreply.github.com>
2023-10-10 23:15:42 -0400

    Create TextContainer.svelte

---
+++ src/components/TextContainer.svelte
@@ -0,0 +1,15 @@
+<div class = "flex justify-center my-20"> <!--Container that holds the text in the center-->
+    <div class = "w-3/4">
+        <h1 class = "font-bold text-5xl">The Importance of Robotics in High School Education</h1> <!--Header-->
+        <p class = "my-4 text-neutral-500 my-5">VPCI Robotics • Tue Oct 10 2023 • #education</p>
+        <h6 class = "font-bold text-lg">Lorem ipsum dolor sit amet</h6> <!--Subtitle 1-->
+        <!--Paragraph 1-->
+        <p class = "my-2">consectetur adipiscing elit. Praesent augue leo, sodales vel diam et, accumsan congue lectus. Fusce non elit turpis. Etiam non nibh nibh. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vestibulum eleifend vestibulum magna, non luctus tortor eleifend in. Cras non sapien fermentum, tempor tellus non, gravida purus. Nunc placerat metus ante, eget ultrices orci aliquet eget. Morbi imperdiet, arcu vitae fermentum tincidunt, erat sapien dignissim nunc, non sollicitudin nulla diam sed tortor. In hac habitasse platea dictumst. Cras sit amet nisi elit. Sed nec lorem nibh. Morbi id pharetra velit.</p>
+        <h6 class = "font-bold text-lg">Vestibulum eleifend odio nulla</h6> <!--Subtitle 2-->
+        <!--Paragraph 2-->
+        <p class = "my-2">quis tempus arcu sodales sed. Vestibulum commodo enim in leo tincidunt, eget pulvinar justo posuere. Morbi elementum odio et maximus ornare. Sed porta feugiat pretium. Vivamus aliquam dui nisi, vitae interdum sapien egestas ullamcorper. Fusce interdum feugiat dolor, id faucibus lorem finibus sed. Nam urna nisi, sodales et tincidunt lobortis, condimentum at quam. Nulla quis mi tristique, pellentesque velit nec, interdum quam. Ut pulvinar eget ipsum eget auctor. Vestibulum fringilla nibh quis elit tincidunt, sodales molestie dolor congue. Etiam eget lobortis justo. Pellentesque vitae leo vitae dui eleifend accumsan sed vitae leo. Proin at porta mauris. Praesent sem eros, vestibulum nec nisi vitae, convallis dapibus velit. Vivamus mattis mollis eros, non volutpat enim ultrices vitae. Praesent sit amet augue nec libero semper rhoncus.</p>
+        <h6 class = "font-bold text-lg">Nulla facilisi</h6> <!--Subtitle 3-->
+        <!--Paragraph 3-->
+        <p class = "my-2">Praesent et semper purus, non pellentesque mi. Fusce eros turpis, accumsan eget urna eu, fringilla tincidunt neque. Aenean tincidunt massa leo, ut vehicula lorem cursus ac. Cras vel eros elementum, lobortis tellus eu, scelerisque ipsum. Donec sed vehicula nunc, ac pretium lorem. Suspendisse mollis, risus eu mollis bibendum, mi nulla convallis arcu, nec scelerisque lacus augue at turpis.</p>
+    </div>
+</div>
