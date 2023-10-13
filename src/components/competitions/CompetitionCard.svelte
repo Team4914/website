b0a9f300c491725d9249f01201d611fc2ac36d37
Sleepyleaf58 <frank.huang2@student.tdsb.on.ca>
2023-10-12 20:04:17 -0400

    Renaming Competition Pages
    Co-authored-by: Me da Ghost <CLS-Ghost350@users.noreply.github.com>

--- src/components/competitions/CompetitionCard.svelte
+++ src/components/competitions/CompetitionCard.svelte
@@ -1,25 +1,22 @@
 <script lang="ts">
-    export let title:string; // Card Title
-    export let videoURL:string; // Video Link
-    export let content:string; // Card Content
+    // props
+    export let img: string;
+    export let imgAlt: string; 
+    export let title: string;
+    export let link: string;
+    export let description: string;
 </script>
 
-<div
-    class="flex flex-row h-52 w-5/12 m-5 bg-slate-300 transition ease-in-out delay-150 hover:bg-slate-200 hover:scale-[1.03] hover:drop-shadow-[0_10px_10px_rgba(250,250,250,0.25)] bg-white border shadow-sm rounded-xl dark:bg-gray-800 dark:border-gray-700 dark:shadow-slate-700/[.7]"
->
-    <iframe
-        class="m-none mt-none w-2/5 h-52 border rounded-l-xl"
-        src="{videoURL}"
-        title="Youtube Video"
-        frameborder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen>  
-    </iframe>
-    <div class="w-3/5 ml-5">
-        <h1 class="text-center text-4xl font-bold text-gray-800 dark:text-white">
-            {title}
-        </h1>
-        <p class="mt-2 text-gray-800 dark:text-gray-400">
-            {content}
-        </p>
+<div class="card card-side bg-base-100 shadow-xl">
+    <figure><img src={img} alt={imgAlt} class="h-full"/></figure> <!-- Image -->
+
+    <div class="card-body">
+        <h2 class="card-title text-primary text-2xl"><a href={link}>{title}</a></h2>
+        <p>{description}</p>
+
+        <!-- If we want to add a button in the future -->
+        <!-- <div class="card-actions justify-end">
+            <button class="btn btn-primary">Listen</button>
+        </div> -->
     </div>
 </div>
