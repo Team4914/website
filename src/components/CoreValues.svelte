25f2498cbac7128a9d3665d56c52f5eda6eee38e
LiuJack342 <136608496+LiuJack342@users.noreply.github.com>
2023-10-17 16:44:50 -0400

    Update CoreValues.svelte

--- src/components/CoreValues.svelte
+++ src/components/CoreValues.svelte
@@ -78,23 +78,23 @@ To change the position of the image inside of the buttons upon being hovered ove
                 <!--Button corresponding to the "Spirited" web page-->
                 <a
                     class="transition-all duration-700 ease-in-out grid h-20 mx-4 my-2 flex-grow card bg-base-300 rounded-box place-items-center bg-[length:450px_350px] bg-[center_bottom_-2rem] bg-[url(var(--firstBoxImage))] opacity-40 btn hover:-translate-y-2 hover:bg-[length:400px_300px] hover:opacity-100 hover:bg-[center_bottom_-2rem]"
-                    href={linkFirstBox}>{firstBoxText}</a
-                >
+                    href={linkFirstBox}>{firstBoxText}
+                </a>
                 <!--Button corresponding to the "Team Oriented" web page-->
                 <a
                     class="transition-all duration-700 ease-in-out grid h-20 mx-4 my-2 flex-grow card bg-base-300 rounded-box place-items-center bg-[length:450px_350px] bg-[center_bottom_-2rem] bg-[url(var(--secondBoxImage))] opacity-40 btn hover:-translate-y-2 hover:bg-[length:400px_300px] hover:opacity-100 hover:bg-[center_bottom_-2rem]"
-                    href={linkSecondBox}>{secondBoxText}</a
-                >
+                    href={linkSecondBox}>{secondBoxText}
+                </a>
                 <!--Button corresponding to the "Empathetic" web page-->
                 <a
                     class="transition-all duration-700 ease-in-out grid h-20 mx-4 my-2 flex-grow card bg-base-300 rounded-box place-items-center bg-[length:300px_150px] bg-[center_bottom_-2rem] bg-[url("https://static1.srcdn.com/wordpress/wp-content/uploads/2021/03/Genshin-Impact-Primogems.png.jpg")] opacity-40 btn hover:-translate-y-2 hover:bg-[length:200px_100px] hover:opacity-100 hover:bg-[center_bottom_-1rem]"
-                    href={linkThirdBox}>{thirdBoxText}</a
-                >
+                    href={linkThirdBox}>{thirdBoxText}
+                </a>
                 <!--Button corresponding to the "Methodical" web page-->
                 <a
                     class="transition-all duration-700 ease-in-out grid h-20 mx-4 my-2 flex-grow card bg-base-300 rounded-box place-items-center bg-[length:450px_350px] bg-[center_bottom_-8rem] bg-[url("https://static1.srcdn.com/wordpress/wp-content/uploads/2021/03/Genshin-Impact-Primogems.png.jpg")] opacity-40 btn hover:-translate-y-2 hover:bg-[length:400px_300px] hover:opacity-100 hover:bg-[center_bottom_-7rem]"
-                    href={linkFourthBox}>{fourthBoxText}</a
-                >
+                    href={linkFourthBox}>{fourthBoxText}
+                </a>
             </div>
         </div>
     </div>
