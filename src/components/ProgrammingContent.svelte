71d695e0c1e51c36cf96cd5d0aae7f86dd63056a
GoyalRitvik <125717243+GoyalRitvik@users.noreply.github.com>
2023-10-21 02:34:13 -0400

    Create ProgrammingContent.svelte

---
+++ src/components/ProgrammingContent.svelte
@@ -0,0 +1,47 @@
+<div class="bg-base-200 min-h-96 flex flex-col items-center justify-center p-10">
+    <!-- Main Card -->
+    <div class="card card-side bg-base-100 mb-4 flex-col p-4 shadow-xl lg:flex-row">
+        <figure class="basis-3/4">
+            <img class="max-w-sm shadow-2xl" src="https://assets.entrepreneur.com/content/3x2/2000/20200429211042-GettyImages-1164615296.jpeg?format=pjeg&auto=webp&crop=1:1" alt="Movie" />
+        </figure>
+        <div class="card-body">
+            <h1 class="text-5xl font-bold">Programming Subteam</h1>
+            <p class="py-6">At team 4914, our programming subteam uses Java and Python to code the robot, which includes creating an intuitive and assistive control system for the drive team, as well as efficiently and safely utilizing the electronics. Being a programmer at 4914 requires you to understand how the entire robot functions as a system, so getting involved with the other technical sub-teams (build, design, electronics) is encouraged. Although prior programming experience is useful, the willingness to learn is more important.</p>
+        </div>
+    </div>
+
+    <!-- Design Process Heading -->
+    <div class="py-6"></div>
+        <div style="border-top: .5px solid white; border-right: .5px solid white;border-left: .5px solid white;" class="shadow-xl bg-base-200 rounded-xl p-6">
+            <h2 class="text-center text-4xl rounded-3xl font-bold mb-4">Ongoing Programming Projects</h2>
+            <p class="bg-base-100 text-lg rounded-lg text-center p-4">We are constantly working to improve our robots' features and capabilities through the code powering it. Below are the ongoing programming projects. If you are interested, contact us via our socials or email.</p>
+        </div>
+    <div class="py-6"></div>
+    <div class="card card-side bg-base-100 mb-4 flex-col p-4 shadow-xl lg:flex-row">
+        <figure class="basis-3/4">
+            <img class="max-w-sm shadow-2xl" src="https://assets.entrepreneur.com/content/3x2/2000/20200429211042-GettyImages-1164615296.jpeg?format=pjeg&auto=webp&crop=1:1" alt="Movie" />
+        </figure>
+        <div class="card-body text-center">
+            <h1 class="text-5xl font-semibold">Robot Vision : Auto-Aim</h1>
+            <p class="py-6">Our robot has an onboard camera running OpenCV on a Raspberry Pi to help automatically align the robot's ball shooter with the target goal. At the moment it runs on Python, however, we are looking to improve its efficiency. Some prior knowledge about Python, C++ or general computer vision processing is useful but not necessary.</p>
+        </div>
+        <figure class="basis-3/4">
+            <img class="max-w-sm shadow-2xl" src="https://assets.entrepreneur.com/content/3x2/2000/20200429211042-GettyImages-1164615296.jpeg?format=pjeg&auto=webp&crop=1:1" alt="Movie" />
+        </figure>
+    </div>
+    <div class="py-6"></div>
+    <div class="card card-side bg-base-100 mb-4 flex-col p-4 shadow-xl lg:flex-row">
+        <figure class="basis-3/4">
+            <img class="max-w-sm shadow-2xl" src="https://assets.entrepreneur.com/content/3x2/2000/20200429211042-GettyImages-1164615296.jpeg?format=pjeg&auto=webp&crop=1:1" alt="Movie" />
+        </figure>
+        <div class="card-body text-center">
+            <h1 class="text-5xl font-semibold">Robot Vision : Auto-Aim</h1>
+            <p class="py-6">Our robot has an onboard camera running OpenCV on a Raspberry Pi to help automatically align the robot's ball shooter with the target goal. At the moment it runs on Python, however, we are looking to improve its efficiency. Some prior knowledge about Python, C++ or general computer vision processing is useful but not necessary.</p>
+        </div>
+        <figure class="basis-3/4">
+            <img class="max-w-sm shadow-2xl" src="https://assets.entrepreneur.com/content/3x2/2000/20200429211042-GettyImages-1164615296.jpeg?format=pjeg&auto=webp&crop=1:1" alt="Movie" />
+        </figure>
+    </div>
+
+
+</div>
