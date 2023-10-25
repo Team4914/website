29013aea20d65b9838dd4f29431168130a6af65f
ShanmugarajahShajeenth <130683557+ShanmugarajahShajeenth@users.noreply.github.com>
2023-10-25 14:53:32 -0400

    Create OutreachShowcase.svelte

---
+++ src/components/OutreachShowcase.svelte
@@ -0,0 +1,59 @@
+<div class="flex flex-wrap justify-evenly">
+<div class="card bg-base-200 mx-10 my-10 shadow-xl hover:scale-105 md:w-3/12">
+    <figure>
+    <img
+        src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+        alt="Shoes"
+    />
+    </figure>
+    <div class="card-body">
+    <h1 class="card-title">Sample</h1>
+    <p>
+        sample text
+    </p>
+    </div>
+</div>
+<div class="card bg-base-200 mx-10 my-10 shadow-xl hover:scale-105 md:w-3/12">
+    <figure>
+    <img
+        src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+        alt="Shoes"
+    />
+    </figure>
+    <div class="card-body">
+    <h1 class="card-title">Sample</h1>
+    <p>
+        sample text
+    </p>
+    </div>
+</div>
+<div class="card bg-base-200 mx-10 my-10 shadow-xl hover:scale-105 md:w-3/12">
+    <figure>
+    <img
+        src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+        alt="Shoes"
+    />
+    </figure>
+    <div class="card-body">
+    <h1 class="card-title">Sample</h1>
+    <p>
+        sample text
+    </p>
+    </div>
+</div>
+<div class="card bg-base-200 mx-10 my-10 shadow-xl hover:scale-105 md:w-3/12">
+    <figure>
+    <img
+        src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+        alt="Shoes"
+    />
+    </figure>
+    <div class="card-body">
+    <h1 class="card-title">Sample</h1>
+    <p>
+        sample text
+    </p>
+    </div>
+</div>
+</div>
+
