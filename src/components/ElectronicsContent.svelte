224fd3bf84227eaf2941372c65584df1123bba4f
LiuJack342 <136608496+LiuJack342@users.noreply.github.com>
2023-10-28 15:08:51 -0400

    Create ElectronicsContent.svelte

---
+++ src/components/ElectronicsContent.svelte
@@ -0,0 +1,19 @@
+<!--This contains all the components of the Electronics subteam page.-->
+
+<script lang="ts">
+    
+</script>
+
+<div class = "my-10">
+    <div class="card card-side bg-base-100 shadow-xl">
+        <div class = "w-1/2">
+            <figure class = "w-1/2"><img src="https://cdn.discordapp.com/attachments/1122723830973075506/1124830665406427136/KSapPirZBfeA5XzLS93LvqELu93MKsGGbgHNDZjyDOl5xWMgQ9YKh9unNuczPVZEQjS73zUoJOADjixgQa8G0rLsrwWIBtMBGY-58r_fvJpas4iww1280.png" style = "height: 300px; width: 500px;" alt="Movie"/></figure>
+        </div>
+        <div class="card-body w-1/2">
+            <h2 class="card-title">ELECTRICAL SUBTEAM</h2>
+            <p>The Electronics team takes the assembled robot and connects the various motors, pistons and any other vision or movement device to their respective controllers. We enable the robot to be controlled by our Drive Team, as well as autonomously during a match. We are responsible not only for designing, connecting, and troubleshooting the electrical and pneumatic components, but also for working together with the Build and Programming Teams to eventually build a fully functioning robot.</p>
+            <br>
+            <p>Members don't need any prerequisite knowledge in electronics as we will provide you with resources and workshops for you to learn the necessary skills, but enthusiam and willingness to learn are important characteristics.</p>
+        </div>
+    </div>
+</div>
