ba920099865665d83bf84d3b2ecc5a33847c1df9
LiuJack342 <136608496+LiuJack342@users.noreply.github.com>
2023-10-28 15:07:49 -0400

    Create ElectronicsGuide.svelte

---
+++ src/components/ElectronicsGuide.svelte
@@ -0,0 +1,49 @@
+<!--This is the electronics guide component on the electronics subteam page-->
+
+<script lang="ts">
+    let linkFirstBox = "asd";
+    let linkSecondBox = "asd";
+    let linkThirdBox = "asd";
+    let linkFourthBox = "asd";
+
+    let firstBoxText = "Spirited";
+    let secondBoxText = "Team Oriented";
+    let thirdBoxText = "Empathetic";
+    let fourthBoxText = "Methodical";
+</script>
+
+<main>
+    <div class="pb-7 bg-base-300 rounded-box">
+        <!--Creates the heading for the section-->
+        <div
+            class="grid h-20 card place-items-center"
+        >
+            <p class = "sm:text-xl md:text-3xl">How to get Started</p>
+            <p class = "sm:text-base md:text-lg">This is example text</p>
+        </div>
+
+        <!--Creates the buttons and dividers for the Core Values section -->
+        <div class="flex flex-wrap w-full">
+            <!--Button corresponding to the "Spirited" web page-->
+            <a
+                class="transition-all duration-700 ease-in-out grid h-20 mx-4 my-2 flex-grow card bg-base-300 rounded-box place-items-center bg-[length:450px_350px] bg-[center_bottom_-2rem] bg-[url('https://th.bing.com/th/id/OIP.2mggqeJ4NZV5yiwdvcq5YwHaFj?pid=ImgDet&rs=1')] opacity-40 btn hover:-translate-y-2 hover:bg-[length:400px_300px] hover:opacity-100 hover:bg-[center_bottom_-2rem]"
+                href={linkFirstBox}>{firstBoxText}
+            </a>
+            <!--Button corresponding to the "Team Oriented" web page-->
+            <a
+                class="transition-all duration-700 ease-in-out grid h-20 mx-4 my-2 flex-grow card bg-base-300 rounded-box place-items-center bg-[length:450px_350px] bg-[center_bottom_-2rem] bg-[url('https://cdn.britannica.com/22/215522-050-8315BB78/green-grass-close-up.jpg')] opacity-40 btn hover:-translate-y-2 hover:bg-[length:400px_300px] hover:opacity-100 hover:bg-[center_bottom_-2rem]"
+                href={linkSecondBox}>{secondBoxText}
+            </a>
+            <!--Button corresponding to the "Empathetic" web page-->
+            <a
+                class="transition-all duration-700 ease-in-out grid h-20 mx-4 my-2 flex-grow card bg-base-300 rounded-box place-items-center bg-[length:300px_150px] bg-[center_bottom_-2rem] bg-[url('https://static1.srcdn.com/wordpress/wp-content/uploads/2021/03/Genshin-Impact-Primogems.png.jpg')] opacity-40 btn hover:-translate-y-2 hover:bg-[length:200px_100px] hover:opacity-100 hover:bg-[center_bottom_-1rem]"
+                href={linkThirdBox}>{thirdBoxText}
+            </a>
+            <!--Button corresponding to the "Methodical" web page-->
+            <a
+                class="transition-all duration-700 ease-in-out grid h-20 mx-4 my-2 flex-grow card bg-base-300 rounded-box place-items-center bg-[length:450px_350px] bg-[center_bottom_-8rem] bg-[url('https://static1.srcdn.com/wordpress/wp-content/uploads/2021/03/Genshin-Impact-Primogems.png.jpg')] opacity-40 btn hover:-translate-y-2 hover:bg-[length:400px_300px] hover:opacity-100 hover:bg-[center_bottom_-7rem]"
+                href={linkFourthBox}>{fourthBoxText}
+            </a>
+        </div>
+    </div>
+</main>
