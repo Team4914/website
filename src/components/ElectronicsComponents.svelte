5c4d66cdb8d8ae82bfc23b51eb2c24eb561352cd
LiuJack342 <136608496+LiuJack342@users.noreply.github.com>
2023-10-30 18:55:48 -0400

    Create ElectronicsComponents.svelte

---
+++ src/components/ElectronicsComponents.svelte
@@ -0,0 +1,38 @@
+<!--This is the electronics guide component on the electronics subteam page-->
+
+<script lang="ts">
+    
+</script>
+
+<main>
+    <div class="carousel w-full">
+        <div id="slide1" class="carousel-item relative w-full">
+            <img src="https://static1.srcdn.com/wordpress/wp-content/uploads/2021/03/Genshin-Impact-Primogems.png.jpg" class="w-full" />
+            <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide4" class="btn btn-circle">❮</a> 
+            <a href="#slide2" class="btn btn-circle">❯</a>
+            </div>
+        </div> 
+        <div id="slide2" class="carousel-item relative w-full">
+            <img src="https://static1.srcdn.com/wordpress/wp-content/uploads/2021/03/Genshin-Impact-Primogems.png.jpg" class="w-full" />
+            <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide1" class="btn btn-circle">❮</a> 
+            <a href="#slide3" class="btn btn-circle">❯</a>
+            </div>
+        </div> 
+        <div id="slide3" class="carousel-item relative w-full">
+            <img src="https://static1.srcdn.com/wordpress/wp-content/uploads/2021/03/Genshin-Impact-Primogems.png.jpg" class="w-full" />
+            <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide2" class="btn btn-circle">❮</a> 
+            <a href="#slide4" class="btn btn-circle">❯</a>
+            </div>
+        </div> 
+        <div id="slide4" class="carousel-item relative w-full">
+            <img src="https://static1.srcdn.com/wordpress/wp-content/uploads/2021/03/Genshin-Impact-Primogems.png.jpg" class="w-full" />
+            <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide3" class="btn btn-circle">❮</a> 
+            <a href="#slide1" class="btn btn-circle">❯</a>
+            </div> nbbnh                                                                                                                                                                                                       .           .////////////////////////////////////////............................................................................................./////////////////////////////.//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        </div>
+    </div>
+</main>
