432dd0514e89e442b4bb4843d321e8271646a3ce
LiuJack342 <136608496+LiuJack342@users.noreply.github.com>
2023-10-30 21:00:08 -0400

    Update ElectronicsComponents.svelte

--- src/components/ElectronicsComponents.svelte
+++ src/components/ElectronicsComponents.svelte
@@ -5,34 +5,47 @@
 </script>
 
 <main>
-    <div class="carousel w-full">
+    <p class = "sm:text-3xl md:text-4xl text-center py-2">Major Components</p>
+    <div class="carousel w-full h-96">
         <div id="slide1" class="carousel-item relative w-full">
-            <img src="https://static1.srcdn.com/wordpress/wp-content/uploads/2021/03/Genshin-Impact-Primogems.png.jpg" class="w-full" />
+            <div class = "py-5">
+                <img class = "absolute w-1/2 left-1/4" src="https://static1.srcdn.com/wordpress/wp-content/uploads/2021/03/Genshin-Impact-Primogems.png.jpg"/>
+                <p class = "absolute w-3/4 left-20 top-2/3">If you need to use a one-off top/right/bottom/left value that doesn’t make sense to include in your theme, use square brackets to generate a property on the fly using any arbitrary value.</p>
+            </div>
             <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-            <a href="#slide4" class="btn btn-circle">❮</a> 
-            <a href="#slide2" class="btn btn-circle">❯</a>
+                <a href="#slide4" class="btn btn-circle">❮</a> 
+                <a href="#slide2" class="btn btn-circle">❯</a>
             </div>
         </div> 
         <div id="slide2" class="carousel-item relative w-full">
-            <img src="https://static1.srcdn.com/wordpress/wp-content/uploads/2021/03/Genshin-Impact-Primogems.png.jpg" class="w-full" />
+            <div class = "py-5">
+                <img class = "absolute w-1/2 left-1/4" src="https://static1.srcdn.com/wordpress/wp-content/uploads/2021/03/Genshin-Impact-Primogems.png.jpg"/>
+                <p class = "absolute w-3/4 left-20 top-2/3">If you need to use a one-off top/right/bottom/left value that doesn’t make sense to include in your theme, use square brackets to generate a property on the fly using any arbitrary value.</p>
+            </div>
             <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-            <a href="#slide1" class="btn btn-circle">❮</a> 
-            <a href="#slide3" class="btn btn-circle">❯</a>
+                <a href="#slide1" class="btn btn-circle">❮</a> 
+                <a href="#slide3" class="btn btn-circle">❯</a>
             </div>
         </div> 
         <div id="slide3" class="carousel-item relative w-full">
-            <img src="https://static1.srcdn.com/wordpress/wp-content/uploads/2021/03/Genshin-Impact-Primogems.png.jpg" class="w-full" />
+            <div class = "py-5">
+                <img class = "absolute w-1/2 left-1/4" src="https://static1.srcdn.com/wordpress/wp-content/uploads/2021/03/Genshin-Impact-Primogems.png.jpg"/>
+                <p class = "absolute w-3/4 left-20 top-2/3">If you need to use a one-off top/right/bottom/left value that doesn’t make sense to include in your theme, use square brackets to generate a property on the fly using any arbitrary value.</p>
+            </div>
             <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-            <a href="#slide2" class="btn btn-circle">❮</a> 
-            <a href="#slide4" class="btn btn-circle">❯</a>
+                <a href="#slide2" class="btn btn-circle">❮</a> 
+                <a href="#slide4" class="btn btn-circle">❯</a>
             </div>
         </div> 
         <div id="slide4" class="carousel-item relative w-full">
-            <img src="https://static1.srcdn.com/wordpress/wp-content/uploads/2021/03/Genshin-Impact-Primogems.png.jpg" class="w-full" />
+            <div class = "py-5 h-500 w-full">
+                <img class = "absolute w-1/2 left-1/4" src="https://static1.srcdn.com/wordpress/wp-content/uploads/2021/03/Genshin-Impact-Primogems.png.jpg"/>
+                <p class = "absolute w-3/4 left-20 top-2/3">If you need to use a one-off top/right/bottom/left value that doesn’t make sense to include in your theme, use square brackets to generate a property on the fly using any arbitrary value.</p>
+            </div>
             <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-            <a href="#slide3" class="btn btn-circle">❮</a> 
-            <a href="#slide1" class="btn btn-circle">❯</a>
-            </div> nbbnh                                                                                                                                                                                                       .           .////////////////////////////////////////............................................................................................./////////////////////////////.//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+                <a href="#slide3" class="btn btn-circle">❮</a> 
+                <a href="#slide1" class="btn btn-circle">❯</a>
+            </div>
         </div>
     </div>
 </main>
