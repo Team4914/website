1fc4c74f6706718d8607f35891c3cda02ccfc359
LiuJack342 <136608496+LiuJack342@users.noreply.github.com>
2023-12-11 22:06:51 +0000

    Created resources page

---
+++ src/components/Resources.svelte
@@ -0,0 +1,46 @@
+<!--This contains the content for the resources page-->
+<!--Note, I was unable to alter the padding and type of unordered lists with the tailwind styles/classes. 
+Fix if possible-->
+
+<!--This seems to be the case with many tailwind classes-->
+
+<script lang="ts">
+</script>
+
+<div class = "p-10">
+    <h2 class = "text-3xl">Resources:</h2>
+    <br>
+
+    <!--Build team resources-->
+    <div class = "rounded-lg p-5 bg-base-300 text-lg">
+        <h3 class = "text-2xl">Build team</h3>
+
+        <ul style = "list-style-type:disc; padding-left:60px;line-height:40px;">
+            <li><a>Mechanical guide</a></li>
+            <li><a>Material guide</a></li>
+            <li><a>asdfghj</a></li>
+        </ul>
+    </div>
+    <br>
+
+    <!-- Web developement resources-->
+    <div class = "rounded-lg p-5 bg-base-300 text-lg">
+        <h3 class = "text-2xl">Programming</h3>
+        <ul style = "list-style-type:disc; padding-left:60px;line-height:40px;">
+            <li><a>Guide</a></li>
+            <li><a>Guide</a></li>
+            <li><a>Guide</a></li>
+        </ul>
+    </div>
+    <br>
+
+    <!--Programming resources-->
+    <div class = "rounded-lg p-5 bg-base-300 text-lg">
+        <h3 class = "text-2xl">Programming</h3>
+        <ul style = "list-style-type:disc; padding-left:60px;line-height:40px;">
+            <li><a>Guide</a></li>
+            <li><a>Guide</a></li>
+            <li><a>Guide</a></li>
+        </ul>
+    </div>
+</div>
