6a48e5b5174db1a9fdd50f87d8e48577745f8085
LiuJack342 <136608496+LiuJack342@users.noreply.github.com>
2023-12-12 21:12:45 +0000

    minor electronics quick fix and finished ressource page

--- src/components/ElectronicsComponents.svelte
+++ src/components/ElectronicsComponents.svelte
@@ -10,7 +10,7 @@
         <div id="slide1" class="carousel-item relative w-full">
             <div class = "py-5">
                 <!--Use the left-[] value to change alignmen-->
-                <img class = "absolute h-2/3 left-[430px]" src="https://static1.srcdn.com/wordpress/wp-content/uploads/2021/03/Genshin-Impact-Primogems.png.jpg"/>
+                <img class = "absolute h-2/3 left-1/3" src="https://static1.srcdn.com/wordpress/wp-content/uploads/2021/03/Genshin-Impact-Primogems.png.jpg"/>
                 <p class = "absolute w-3/4 left-20 top-3/4">If you need to use a one-off top/right/bottom/left value that doesn’t make sense to include in your theme, use square brackets to generate a property on the fly using any arbitrary value.</p>
             </div>
             <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
@@ -20,7 +20,7 @@
         </div> 
         <div id="slide2" class="carousel-item relative w-full">
             <div class = "py-5">
-                <img class = "absolute h-2/3 left-[430px]" src="https://static1.srcdn.com/wordpress/wp-content/uploads/2021/03/Genshin-Impact-Primogems.png.jpg"/>
+                <img class = "absolute h-2/3 left-1/3" src="https://static1.srcdn.com/wordpress/wp-content/uploads/2021/03/Genshin-Impact-Primogems.png.jpg"/>
                 <p class = "absolute w-3/4 left-20 top-3/4">If you need to use a one-off top/right/bottom/left value that doesn’t make sense to include in your theme, use square brackets to generate a property on the fly using any arbitrary value.</p>
             </div>
             <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
@@ -30,7 +30,7 @@
         </div> 
         <div id="slide3" class="carousel-item relative w-full">
             <div class = "py-5">
-                <img class = "absolute h-2/3 left-[430px]" src="https://static1.srcdn.com/wordpress/wp-content/uploads/2021/03/Genshin-Impact-Primogems.png.jpg"/>
+                <img class = "absolute h-2/3 left-1/3" src="https://static1.srcdn.com/wordpress/wp-content/uploads/2021/03/Genshin-Impact-Primogems.png.jpg"/>
                 <p class = "absolute w-3/4 left-20 top-3/4">If you need to use a one-off top/right/bottom/left value that doesn’t make sense to include in your theme, use square brackets to generate a property on the fly using any arbitrary value.</p>
             <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
                 <a href="#slide2" class="btn btn-circle">❮</a> 
@@ -39,7 +39,7 @@
         </div> 
         <div id="slide4" class="carousel-item relative w-full">
             <div class = "py-5 h-500 w-full">
-                <img class = "absolute h-2/3 left-[430px]" src="https://static1.srcdn.com/wordpress/wp-content/uploads/2021/03/Genshin-Impact-Primogems.png.jpg"/>
+                <img class = "absolute h-2/3 left-1/3" src="https://static1.srcdn.com/wordpress/wp-content/uploads/2021/03/Genshin-Impact-Primogems.png.jpg"/>
                 <p class = "absolute w-3/4 left-20 top-3/4">If you need to use a one-off top/right/bottom/left value that doesn’t make sense to include in your theme, use square brackets to generate a property on the fly using any arbitrary value.</p>
             </div>
             <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
--- src/components/Resources.svelte
+++ src/components/Resources.svelte
@@ -7,6 +7,14 @@ Fix if possible-->
 <script lang="ts">
 </script>
 
+<!--creating a class to make links visible again-->
+<style>
+    .link{
+        text-decoration: underline;
+        color:  rgb(30 58 138);
+    }
+</style>
+
 <div class = "p-10">
     <h2 class = "text-3xl">Resources:</h2>
     <br>
@@ -16,9 +24,9 @@ Fix if possible-->
         <h3 class = "text-2xl">Build team</h3>
 
         <ul style = "list-style-type:disc; padding-left:60px;line-height:40px;">
-            <li><a>Mechanical guide</a></li>
-            <li><a>Material guide</a></li>
-            <li><a>asdfghj</a></li>
+            <li><a class = "link" href="">Mechanical guide</a></li>
+            <li><a class = "link" href="">Material guide</a></li>
+            <li><a class = "link" href="">asdfghj</a></li>
         </ul>
     </div>
     <br>
@@ -27,9 +35,9 @@ Fix if possible-->
     <div class = "rounded-lg p-5 bg-base-300 text-lg">
         <h3 class = "text-2xl">Programming</h3>
         <ul style = "list-style-type:disc; padding-left:60px;line-height:40px;">
-            <li><a>Guide</a></li>
-            <li><a>Guide</a></li>
-            <li><a>Guide</a></li>
+            <li><a class = "link" href="">Guide</a></li>
+            <li><a class = "link" href="">Guide</a></li>
+            <li><a class = "link" href="">Guide</a></li>
         </ul>
     </div>
     <br>
@@ -38,9 +46,9 @@ Fix if possible-->
     <div class = "rounded-lg p-5 bg-base-300 text-lg">
         <h3 class = "text-2xl">Programming</h3>
         <ul style = "list-style-type:disc; padding-left:60px;line-height:40px;">
-            <li><a>Guide</a></li>
-            <li><a>Guide</a></li>
-            <li><a>Guide</a></li>
+            <li><a class = "link" href="">Guide</a></li>
+            <li><a class = "link" href="">Guide</a></li>
+            <li><a class = "link" href="">Guide</a></li>
         </ul>
     </div>
 </div>
