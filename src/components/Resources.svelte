2a572ce0f791e355ca878aac3224a95a4c7ff6b7
LiuJack342 <136608496+LiuJack342@users.noreply.github.com>
2023-12-14 02:34:11 +0000

    Completed resources design, todo: add new resources

--- src/components/Resources.svelte
+++ src/components/Resources.svelte
@@ -20,35 +20,56 @@ Fix if possible-->
     <br>
 
     <!--Build team resources-->
-    <div class = "rounded-lg p-5 bg-base-300 text-lg">
+    <div class = "rounded-lg p-5 bg-base-300 text-lg transition-all duration-500 hover:translate-x-3">
         <h3 class = "text-2xl">Build team</h3>
 
         <ul style = "list-style-type:disc; padding-left:60px;line-height:40px;">
-            <li><a class = "link" href="">Mechanical guide</a></li>
-            <li><a class = "link" href="">Material guide</a></li>
-            <li><a class = "link" href="">asdfghj</a></li>
+            <li><a class = "link transition-all duration-500 hover:text-xl" href="https://docs.google.com/presentation/d/1D-TTWkGcIM9IYwn_SvGX0fwiERCBiqFc/edit?rtpof=true&sd=true">Mechanical guide</a></li>
+            <li><a class = "link transition-all duration-500 hover:text-xl" href="https://docs.google.com/presentation/d/1XWlT2D6pqDgk782QwVp3kUptwbJLyZR2/edit">Material guide</a></li>
         </ul>
     </div>
     <br>
 
-    <!-- Web developement resources-->
-    <div class = "rounded-lg p-5 bg-base-300 text-lg">
-        <h3 class = "text-2xl">Programming</h3>
+    <!--Electrionics resources-->
+    <div class = "rounded-lg p-5 bg-base-300 text-lg transition-all duration-500 hover:translate-x-3">
+        <h3 class = "text-2xl">Electronics</h3>
         <ul style = "list-style-type:disc; padding-left:60px;line-height:40px;">
-            <li><a class = "link" href="">Guide</a></li>
-            <li><a class = "link" href="">Guide</a></li>
-            <li><a class = "link" href="">Guide</a></li>
+            <li><a class = "link transition-all duration-500 hover:text-xl" href="https://drive.google.com/file/d/1EDPonK7Pc_Vf_9crYTMUXauCYa4-7a5h/view">General Electronics System Notes</a></li>
+            <li><a class = "link transition-all duration-500 hover:text-xl" href="https://mililanirobotics.gitbooks.io/frc-electrical-bible/content/">Additional System Notes</a></li>
+            <li><a class = "link transition-all duration-500 hover:text-xl" href="https://wpilib.screenstepslive.com/s/currentCS/m/kop/l/1030226-wiring-the-frc-control-system">Wiring the FRC Control System</a></li>
+            <li><a class = "link transition-all duration-500 hover:text-xl" href="https://firstfrc.blob.core.windows.net/frc2017/pneumatics-manual.pdf">FRC Pneumatics Guide</a></li>
         </ul>
     </div>
     <br>
 
-    <!--Programming resources-->
-    <div class = "rounded-lg p-5 bg-base-300 text-lg">
+    <!-- Programming resources-->
+    <div class = "rounded-lg p-5 bg-base-300 text-lg transition-all duration-500 hover:translate-x-3">
         <h3 class = "text-2xl">Programming</h3>
+
+        <h4 class = "text-xl mt-5">Computer Vision</h4>
         <ul style = "list-style-type:disc; padding-left:60px;line-height:40px;">
-            <li><a class = "link" href="">Guide</a></li>
-            <li><a class = "link" href="">Guide</a></li>
-            <li><a class = "link" href="">Guide</a></li>
+            <li><a class = "link transition-all duration-500 hover:text-xl" href="https://ftc-docs.firstinspires.org/en/latest/ftc_ml/">FIRST Machine Learning Toolchain</a></li>
+            <li><a class = "link transition-all duration-500 hover:text-xl" href="https://www.firstinspires.org/resource-library/ftc/game-and-season-info">First Robotics Resource library</a></li>
+            <li><a class = "link transition-all duration-500 hover:text-xl" href="https://www.youtube.com/watch?v=KNAWp2S3w94">Introduction to Machine Learning - Youtube</a></li>
+            <li><a class = "link transition-all duration-500 hover:text-xl" href="https://developers.google.com/machine-learning/crash-course/ml-intro">Introduction to Machine Learning</a></li>
+        </ul>
+
+        <h4 class = "text-xl mt-5">Other</h4>
+        <ul style = "list-style-type:disc; padding-left:60px;line-height:40px;">
+            <li><a class = "link transition-all duration-500 hover:text-xl" href="">Guide</a></li>
+            <li><a class = "link transition-all duration-500 hover:text-xl" href="">Guide</a></li>
+            <li><a class = "link transition-all duration-500 hover:text-xl" href="">Guide</a></li>
+        </ul>
+    </div>
+    <br>
+
+    <!--Web development resources-->
+    <div class = "rounded-lg p-5 bg-base-300 text-lg transition-all duration-500 hover:translate-x-3">
+        <h3 class = "text-2xl">Web development</h3>
+        <ul style = "list-style-type:disc; padding-left:60px;line-height:40px;">
+            <li><a class = "link transition-all duration-500 hover:text-xl" href="">Guide</a></li>
+            <li><a class = "link transition-all duration-500 hover:text-xl" href="">Guide</a></li>
+            <li><a class = "link transition-all duration-500 hover:text-xl" href="">Guide</a></li>
         </ul>
     </div>
 </div>
