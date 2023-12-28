3985f0a4f61560c2f7d2e743f6f3bcacd5df2c27
Surya <tallapaneni.surya@gmail.com>
2023-12-27 22:18:56 -0500

    chore: prettier

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: "https://vicparkrobotics.picafe.me",
-  integrations: [svelte(), sitemap()]
+  integrations: [svelte(), sitemap()],
 });
--- src/components/navbarPageNames.ts
+++ src/components/navbarPageNames.ts
@@ -2,28 +2,27 @@
 // IDK where to put this -- move somewhere else?
 
 type Page = {
-    name: string;
-    href?: string;
-    submenus?: { name: string; href: string }[];
-  };
-  
-  const pages: Page[] = [
-    {
-      name: "Subteams",
-      submenus: [
-        { name: "Design and CAD", href: "/subteams/design" },
-        { name: "Build", href: "/subteams/build" },
-        { name: "Electronics", href: "/subteams/electronics" },
-        { name: "Programming", href: "/subteams/programming" },
-        { name: "Media", href: "/subteams/media" },
-      ],
-    },
-    { name: "Blog", href: "/blog" },
-    { name: "Competitions", href: "/competitions" },
-    { name: "Outreach", href: "/community-outreach" },
-    { name: "Resources", href: "/resources" },
-    { name: "About Us", href: "/about-us" },
-  ];
-  
-  export default pages;
-  
+  name: string;
+  href?: string;
+  submenus?: { name: string; href: string }[];
+};
+
+const pages: Page[] = [
+  {
+    name: "Subteams",
+    submenus: [
+      { name: "Design and CAD", href: "/subteams/design" },
+      { name: "Build", href: "/subteams/build" },
+      { name: "Electronics", href: "/subteams/electronics" },
+      { name: "Programming", href: "/subteams/programming" },
+      { name: "Media", href: "/subteams/media" },
+    ],
+  },
+  { name: "Blog", href: "/blog" },
+  { name: "Competitions", href: "/competitions" },
+  { name: "Outreach", href: "/community-outreach" },
+  { name: "Resources", href: "/resources" },
+  { name: "About Us", href: "/about-us" },
+];
+
+export default pages;
